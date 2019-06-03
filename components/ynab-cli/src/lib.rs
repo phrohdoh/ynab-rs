ff1ed27248fbca0c4354ccdbe37cd774414fe07f
Taryn Hill <taryn@phrohdoh.com>
2019-06-03 13:15:33 -0500

    ynab-cli: Add `output-format` support to `get-all-scheduled-transactions`
    This allows the cli user to specify the format that the data from YNAB should
    be printed in.
    
    The current possible values are:
    - human
    - json
    - json-pretty
    
    If an output format is not specified `human` will be used by default.

--- components/ynab-cli/src/lib.rs
+++ components/ynab-cli/src/lib.rs
@@ -1,4 +1,17 @@
-use uuid::Uuid;
+use std::{
+    str::{
+        FromStr,
+    },
+    io,
+};
+
+use serde::{
+    Deserialize,
+};
+
+use uuid::{
+    Uuid,
+};
 
 use reqwest::{
     Client as HttpClient,
@@ -28,7 +41,29 @@ enum Command {
 
         #[structopt(long = "budget-id")]
         budget_id: Uuid,
+
+        #[structopt(long = "output-format")]
+        output_format: Option<OutputFormat>,
     },
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(rename_all = "kebab-case")]
+enum OutputFormat {
+    /// Rust's pretty-printed debug format
+    Human,
+    /// Machine-readable single-line JSON
+    Json,
+    /// Machine-readable pretty-printed JSON
+    JsonPretty,
+}
+
+impl FromStr for OutputFormat {
+    type Err = serde_json::error::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(serde_json::from_str(&format!("\"{}\"", s))?)
+    }
 }
 
 #[derive(Debug, StructOpt)]
@@ -46,11 +81,28 @@ fn run(opt: Opt) {
     let http_client = HttpClient::new();
 
     match opt.command {
-        Command::GetAllScheduledTransactions { bearer_token_opt, budget_id } => {
+        Command::GetAllScheduledTransactions {
+            bearer_token_opt,
+            budget_id,
+            output_format,
+        } => {
             let client = Client::new(bearer_token_opt.bearer_token);
             let res = client.get_all_scheduled_transactions(&budget_id.to_string(), &http_client);
-            let stxns = res.expect("to get back scheduled transactions");
-            println!("{:#?}", stxns);
+            let stxns = res.expect("to not get an error");
+
+            match output_format {
+                Some(OutputFormat::Json) => {
+                    let _ = serde_json::to_writer(io::stdout().lock(), &stxns);
+                    println!();
+                },
+                Some(OutputFormat::JsonPretty) => {
+                    let _ = serde_json::to_writer_pretty(io::stdout().lock(), &stxns);
+                    println!();
+                },
+                Some(OutputFormat::Human) | _ => {
+                    println!("{:#?}", stxns);
+                },
+            }
         },
     }
 }
